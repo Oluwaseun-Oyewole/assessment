195b239f7d0e38da3c6102dd858f507114113d71
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2023-12-01 13:21:22 +0100

    fixed category breakdown

--- src/modules/dashboard/pages/budget/category.tsx
+++ src/modules/dashboard/pages/budget/category.tsx
@@ -6,46 +6,43 @@ import { ContextCreator } from "../../context";
 import { ContextCreatorType } from "../../context/type";
 
 export const CategoryBreakDown = () => {
-  const { categories, total, amount } = useContext(
-    ContextCreator
-  ) as ContextCreatorType;
+  const { categories } = useContext(ContextCreator) as ContextCreatorType;
 
   return (
     <Box className="py-4  lg:mb-0">
       <Text fontSize="2xl" fontWeight={"600"} py={6}>
         Category Breakdown
       </Text>
-
-      {total < amount && (
-        <Box className="flex flex-col gap-5">
-          {categories?.map((category, index) => {
-            return (
-              <Box key={index} className="flex justify-between">
-                <Box className="flex items-center gap-4">
-                  {ImageUpload(category.title)}
-                  <Box>
-                    <Text className="text-base font-semibold">
-                      {category.title}
-                    </Text>
-                    <Text className="text-sm text-secondary mt-2">
-                      {category.percentage} %
-                    </Text>
-                  </Box>
+      (
+      <Box className="flex flex-col gap-5">
+        {categories?.map((category, index) => {
+          return (
+            <Box key={index} className="flex justify-between">
+              <Box className="flex items-center gap-4">
+                {ImageUpload(category.title)}
+                <Box>
+                  <Text className="text-base font-semibold">
+                    {category.title}
+                  </Text>
+                  <Text className="text-sm text-secondary mt-2">
+                    {category.percentage} %
+                  </Text>
                 </Box>
-                <Text>
-                  <span className="font-semibold">
-                    {`${formatCurrency(Number(category?.amount), `\u20A6`)}`}
-                  </span>
-                  /
-                  <span className="text-secondary">
-                    {`${formatCurrency(Number(category.total), `\u20A6`)}`}
-                  </span>
-                </Text>
               </Box>
-            );
-          })}
-        </Box>
-      )}
+              <Text>
+                <span className="font-semibold">
+                  {`${formatCurrency(Number(category?.amount), `\u20A6`)}`}
+                </span>
+                /
+                <span className="text-secondary">
+                  {`${formatCurrency(Number(category.total), `\u20A6`)}`}
+                </span>
+              </Text>
+            </Box>
+          );
+        })}
+      </Box>
+      )
     </Box>
   );
 };
